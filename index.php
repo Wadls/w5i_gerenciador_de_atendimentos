6565c944f64c821ad1d83f38d986beb34f37540a
Wadls <wadsondaniel9@gmail.com>
2026-05-03 06:03:10 -0300

    Adição dos Requisitos, interpretação dos objetos

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Empresa</title>
+  </head>
+  <body>
+    <div class= "container">
+        <div class= "row">
+            <div class= "column">
+                <div class="p-5 mb-4 bg-light rounded-3">
+                    <div class="container-fluid py-5">
+                        <h1 class="display-4 fw-bold">Cadatro_Php + Mysql</h1>
+                        <p class="col-md-8 fs-4">Esse projeto têm como objetivo, gerenciar o fluxo de chamados de suporte ou solicitações internas, sem o uso de javascript, apenas <b>Hmtl, Php,Bootsrap e Mysql</b></p>
+                        <hr class="my-4">
+                        <p>Acesse as funções</p>
+                        <a href="cadastro.php"f class="btn btn-primary btn-lg" type="button">Criar Chamado<a>
+                        <a href="pesquisa.php"f class="btn btn-primary btn-lg" type="button">Pesquisa<a>
+                        
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+  </body>
+</html>
